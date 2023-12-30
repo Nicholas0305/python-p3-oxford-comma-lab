a0bd6a232976cc5f4b2c8c0b0be20a0ec898750d
Nicholas <najernigin@gmail.com>
2023-12-29 20:47:31 -0600

    Complete Lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,22 @@
 def oxford_comma(items):
-    return None
+    item_string = ""
+    if len(items) == 1:
+        item_string = items[0]
+    elif len(items) == 2:
+        item_string = items[0] + " and "+ items[1]
+    elif len(items) == 3:
+        item_string = items[0] + ', ' + items[1]+ ', and '+items[2]
+    else:
+        counter = 0
+        for item in items:
+            if len(items) - counter != 1:
+                item_string += item + ", "
+                counter += 1
+            else:
+                item_string += "and " + item
+        
+            
+        
+    return item_string
+
+
